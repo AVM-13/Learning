b1f70ce11a2839b2ca2294f5937c9cd47b231992
AVM <anoobvm@gmail.com>
2017-12-09 05:35:11 +0530

    updated home page again

--- home.php
+++ home.php
@@ -1,8 +1,12 @@
+<?php
+$test   =  "learning purpose";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title><?php echo $test; ?></title>
 </head>
 <body>
 	This is the new home page for  testing . Created by avm.
